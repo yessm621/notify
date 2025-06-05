5db6c4de345cfea3aae5345d89bb4d1584ecd735
yessm621 <yessm621@gmail.com>
2025-06-05 16:45:46 +0900

    first commit

---
+++ src/test/java/com/me/notify/NotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.notify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
