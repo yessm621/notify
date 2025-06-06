b3a635f4d0e9179becde8ddf8aa9921f713f13b3
yessm621 <yessm621@gmail.com>
2025-06-06 12:57:59 +0900

    alarm 엔티티의 user 컬럼이 동일하여 오류 발생, 수정

--- src/main/java/com/me/notify/entity/Alarm.java
+++ src/main/java/com/me/notify/entity/Alarm.java
@@ -13,11 +13,11 @@ public class Alarm {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "from_user_id")
     private Users fromUserId; // 댓글 또는 좋아요를 누른 사람 (알림을 발생시킨 사람)
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "target_id")
     private Users targetId;
 
     @Enumerated(EnumType.STRING)
